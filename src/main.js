a235c9135f07a182e9d32803f9b74ef5d507a3c1
Nguyen Dinh Phu <phunguyen06072001@gmail.com>
2023-09-18 11:22:23 +0700

    tạo project, thêm thư viện

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(VueSweetalert2);
+Vue.use(ElementUI, {locale})
+
+Vue.config.productionTip = false
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
