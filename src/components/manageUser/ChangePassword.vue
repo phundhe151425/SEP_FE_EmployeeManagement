6456b5336509e6aace7f40d5c803537c09ef11c3
NDT369 <92255832+NDT369@users.noreply.github.com>
2023-10-05 15:49:07 +0700

    update change password

--- src/components/manageUser/ChangePassword.vue
+++ src/components/manageUser/ChangePassword.vue
@@ -86,7 +86,7 @@ export default {
       }
     };
     return {
-      message:"",
+      message: "",
       ruleForm: {
         email: "",
         oldPassword: "",
@@ -131,9 +131,10 @@ export default {
           DataService.changePass(this.ruleForm)
             .then((response) => {
               console.log(response.data);
+               window.location.replace("http://localhost:2001/manageUser");
             })
             .catch((e) => {
-              this.message = "Mật khẩu cũ không đúng vui lòng nhập lại!"
+              this.message = "Mật khẩu cũ không đúng vui lòng nhập lại!";
               console.log(e);
             });
         } else {
