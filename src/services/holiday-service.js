897948dc731af61472309ca1303b242636829ef2
NDT369 <92255832+NDT369@users.noreply.github.com>
2023-10-18 15:48:09 +0700

    change api holidayManage

--- src/services/holiday-service.js
+++ src/services/holiday-service.js
@@ -1,29 +1,30 @@
 import httpCommon from "@/http-common";
 import { BASE_URL } from "@/http-common";
 import axios from 'axios';
-
+import authHeader from "@/services/auth-header";
 
 class HolidayService {
     save(data) {
-        return axios.post(BASE_URL + "/holiday/create-holiday", data);
+        return axios.post(BASE_URL + "/auth/holiday/create-holiday", data,{headers: authHeader()});
     }
     getData(page, size, search, year) {
-        return httpCommon.get( "/holiday/get-all-holiday?page="+page+"&size="+size+"&search="+search +"&year="+year);
+        return axios.get( BASE_URL+"/auth/holiday/get-all-holiday?page="+page
+        +"&size="+size+"&search="+search +"&year="+year,{headers: authHeader()});
     }
 
     getHoliday(id) {
-        return httpCommon.get(`/holiday/holiday-details/${id}`);
+        return httpCommon.get(`/auth/holiday/holiday-details/${id}`,{headers: authHeader()});
     }
 
     updateHoliday(id, data) {
-        return axios.put(BASE_URL + `/holiday/update-holiday/${id}`,data);
+        return axios.put(BASE_URL + `/auth/holiday/update-holiday/${id}`,data,{headers: authHeader()});
     }
     deleteHoliday(id){
-        return httpCommon.delete(`/holiday/delete-holiday/${id}`);
+        return httpCommon.delete(`/auth/holiday/delete-holiday/${id}`,{headers: authHeader()});
     }
 
     getYears(){
-        return httpCommon.get( "/holiday/get-holidays/get-years");
+        return httpCommon.get( "/auth/holiday/get-holidays/get-years", {headers: authHeader()});
     }
 
 }
